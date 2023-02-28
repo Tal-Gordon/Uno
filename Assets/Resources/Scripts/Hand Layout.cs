c04987c539b38b140c7465a5c94e37eaebe655dd
Tal-Gordon <77861564+Tal-Gordon@users.noreply.github.com>
2023-02-28 23:00:39 +0200

    Cards spacing updates correctly

--- Assets/Resources/Scripts/Hand Layout.cs
+++ Assets/Resources/Scripts/Hand Layout.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class HandLayout : MonoBehaviour
 {
-    public GameObject[] cards;
+    public List<GameObject> cards = new List<GameObject>();
     public float yValue = 1f;
-    public float spacing = 1.5f;
+    public float spacing = 0.67f;
+
+    private readonly float cardSizeX = 1.289f;
     void Start()
     {
-        cards = new GameObject[transform.childCount];
-        for (int i = 0; i < cards.Length; i++)
-        {
-            //cards[i] = transform.GetChild(i).GetComponent<Card>();
-            cards[i] = transform.GetChild(i).gameObject;
-            cards[i].GetComponent<SpriteRenderer>().sortingOrder = i + 1;
-        }
         InvokeRepeating(nameof(UpdateVariables), 0f, 0.25f);
     }
 
@@ -26,38 +21,87 @@ public class HandLayout : MonoBehaviour
 
     void UpdateVariables()
     {
-        for (int i = 0; i < cards.Length; i++)
+        cards.Clear();
+        int amountOfCards = 0;
+
+        foreach (Transform child in transform)
         {
+            if (child.gameObject.activeSelf)
+            {
+                amountOfCards++;
+                cards.Add(child.gameObject);
+            }
+        }
+
+        int sortingOrder = 1;
+        for (int i = 0; i < cards.Count; i++)
+        {
+            cards[i].GetComponent<SpriteRenderer>().sortingOrder = sortingOrder + i;
             cards[i].transform.localPosition = new Vector3(cards[i].transform.position.x, yValue, 0);
         }
-    }
-
-    private void UpdateCardsSpacing()
-    {
-        int numOfCards = cards.Length;
-        float cardSize = cards[0].GetComponent<SpriteRenderer>().size.x;
-        float totalWidth = (cards[cards.GetUpperBound(0)].transform.localPosition.x + cardSize / 2) - (cards[0].transform.localPosition.x - cardSize / 2);
-        float leftmostCardXPosition = cards[0].transform.localPosition.x;
-
-        for (int i = 0; i < cards.Length; i++)
+        if (cards.Count > 1)
         {
-            
+            UpdateCardsSpacing(spacing);
         }
     }
-    private Vector3 GetCardsMeanPosition()
-    {
-        float xPosition = 0f;
-        float yPosition = 0f;
 
-        for (int i = 0; i < cards.Length; i++)
+    private void UpdateCardsSpacing(float spacing)
+    {
+        int numOfCards = cards.Count;
+        int middleCardIndex;
+        float cardsMeanXPosition = GetAnchorXPosition();
+
+        if (numOfCards % 2 == 0)
         {
-            xPosition += cards[i].transform.localPosition.x;
-            yPosition += cards[i].transform.localPosition.y;
+            // Update middle index (left one)
+            middleCardIndex = (numOfCards / 2) - 1;
+
+            // Check if middle cards requires repositioning
+            if (cards[middleCardIndex].transform.localPosition.x - cards[middleCardIndex + 1].transform.localPosition.x != spacing)
+            {
+                cards[middleCardIndex].transform.localPosition = new Vector3(cardsMeanXPosition - (spacing / 2), cards[middleCardIndex].transform.localPosition.y, 0);
+                cards[middleCardIndex + 1].transform.localPosition = new Vector3(cardsMeanXPosition + spacing / 2, cards[middleCardIndex + 1].transform.localPosition.y, 0);
+            }
+
+            // Handle second half of deck
+            for (int i = middleCardIndex + 1; i < cards.Count - 1; i++)
+            {
+                cards[i + 1].transform.localPosition = new Vector3(cards[i].transform.localPosition.x + spacing, cards[i + 1].transform.localPosition.y, 0);
+            }
+
+            // Handle first half of deck
+            for (int i = middleCardIndex; i > 0; i--)
+            {
+                cards[i - 1].transform.localPosition = new Vector3(cards[i].transform.localPosition.x - spacing, cards[i - 1].transform.localPosition.y, 0);
+            }
         }
 
-        xPosition /= cards.Length;
-        yPosition /= cards.Length;
+        else if (numOfCards % 2 != 0)
+        {
+            // Update middle index
+            middleCardIndex = numOfCards / 2;
 
-        return new Vector3(xPosition, yPosition, 0);
+            // Check if middle card requires repositioning
+            if (cards[middleCardIndex].transform.localPosition.x != cardsMeanXPosition)
+            {
+                cards[middleCardIndex].transform.localPosition = new Vector3(cardsMeanXPosition, cards[middleCardIndex].transform.localPosition.y, 0);
+            }
+
+            // Handle second half of deck
+            for (int i = middleCardIndex; i < cards.Count - 1; i++)
+            {
+                cards[i + 1].transform.localPosition = new Vector3(cards[i].transform.localPosition.x + spacing, cards[i + 1].transform.localPosition.y, 0);
+            }
+
+            // Handle first half of deck
+            for (int i = middleCardIndex; i > 0; i--)
+            {
+                cards[i - 1].transform.localPosition = new Vector3(cards[i].transform.localPosition.x - spacing, cards[i + 1].transform.localPosition.y, 0);
+            }
+        }
+    }
+    private float GetAnchorXPosition()
+    {
+        return transform.parent.position.x - (cardSizeX / 2);
     }
 }
